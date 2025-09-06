27d86dd7438fdbb0c16a60fc69f1ddfe260efcd0
usagopro <107623239+usagopro@users.noreply.github.com>
2025-09-06 19:07:30 +0530

    Delete phptutorials directory

--- phptutorials/database_connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dbservername = 'localhost';
-    $dbusername = 'root';
-    $dbpassword = '';
-    $dbname = 'signup';
-
-    $conn = mysqli_connect($dbservername , $dbusername , $dbpassword , $dbname);
-
-?>
--- phptutorials/footer.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<p>Copyright &copy; 1999-" . date("Y") . " Rgukt.com</p>";
-?>
-
